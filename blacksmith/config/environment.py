0c1957fe74ac9c1a7b3716ddeb8dd5069bcd6049
alec <fkalec@gmail.com>
2023-08-12 14:17:02 -0700

    refactor llm_call

--- blacksmith/config/environment.py
+++ blacksmith/config/environment.py
@@ -2,4 +2,3 @@ import os
 
 MODEL = os.getenv("MODEL")
 TEMPERATURE = float(os.getenv("TEMPERATURE"))
-MAX_TOKENS = int(os.getenv("MAX_TOKENS"))
--- blacksmith/tools.py
+++ blacksmith/tools.py
@@ -38,7 +38,12 @@ def tool(name, description, params):
     return decorator
 
 
-def use_tool(tool_name, args):
+def use_tool(tool_name: str, args: dict):
+    """
+    Make an API request to a tool.
+    tool_name should be a string corresponding to the container name of the tool being called
+    args should be a Python Dictionary object
+    """
     return registry.use_tool(tool_name=tool_name, args=args)
 
 
