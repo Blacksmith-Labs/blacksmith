84d87944148a560a83a57be4c0c62e97458d3b78
alec <fkalec@gmail.com>
2023-08-13 13:26:16 -0700

    use new SDK functions for ReAct implementation

--- blacksmith/agents/react.py
+++ blacksmith/agents/react.py
@@ -1,7 +1,4 @@
-import json
-from tenacity import retry, stop_after_attempt
-from blacksmith.llm import llm_call
-from blacksmith.tools import use_tool
+from blacksmith.llm import Conversation
 from blacksmith.config.prompts import DEFAULT_SYSTEM_PROMPT, DEFAULT_REACT_PROMPT
 
 
@@ -20,60 +17,15 @@ class ReActAgent:
 
         self.max_loops = kwargs.get("max_loops", 5)
 
-    def process(self, messages, query):
-        func_calls = []
-
-        @retry(stop=stop_after_attempt(self.max_loops))
-        def _think():
-            resp = llm_call(messages=messages)
-
-            iterations = 0
-            while True:
-                print("Iteration ", iterations)
-                print(resp)
-
-                content = resp.get("content")
-                if content and ("Final Answer:" in content):
-                    print(messages)
-                    final_answer_index = content.index("Final Answer:") + len("Final Answer:")
-                    final_answer = content[final_answer_index:].strip()
-                    return final_answer
-
-                func = resp.get("function_call")
-                if func:
-                    tool_name = func["name"]
-                    args = json.loads(func["arguments"])
-                    # The port is 5000 locally, 80 for k8s, create a function to do this dynamically
-                    data = use_tool(tool_name=tool_name, args=args)
-                    print(f"Executed function {tool_name}. Result: {data}")
-                    func_calls.append(
-                        {
-                            "execution_order": iterations,
-                            "function_name": {tool_name},
-                            "params": {func["arguments"]},
-                            "result": {json.dumps(data)},
-                        }
-                    )
-                    messages.append(
-                        {
-                            "role": "user",
-                            "content": f"""
-                                Result of {tool_name} is {data}.
-                                Generate an observation based on this result.
-                                If you have enough information to answer {query}, return the final answer prefixed with 'Final Answer:'.
-                                Otherwise, based on this observation, proceed with other function calls to gather information as appropriate.
-                            """,
-                        }
-                    )
-                resp = llm_call(messages=messages)
-                iterations += 1
-
-        return _think(), func_calls
-
     def run(self, input: str) -> None:
-        formatted_prompt = self.prompt.format(input=input)
-        messages = [
-            {"role": "system", "content": self.system_prompt},
-            {"role": "user", "content": formatted_prompt},
-        ]
-        return self.process(messages=messages, query=input)
+        c = Conversation(system_prompt=self.system_prompt)
+        response = c.ask(prompt=self.prompt.format(input=input))
+
+        # We know the agent is done when we have a 'content' parameter
+        while response.has_function_call():
+            # Execute the function call
+            result = response.execute_function_call()
+
+            # Let's continue the conversation see if we have enough info to answer the original question.
+            response = c.continue_from_result(result)
+        return response.content
