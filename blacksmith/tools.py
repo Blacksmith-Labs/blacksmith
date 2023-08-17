1eb2e217186b3783eef185edda6d5bda0b989b97
alec <fkalec@gmail.com>
2023-08-17 14:29:39 -0700

    refactor tools

--- blacksmith/tools.py
+++ blacksmith/tools.py
@@ -1,39 +1,10 @@
-import uvicorn
-from fastapi import FastAPI, Request
-import threading
-import functools
 import os
-from blacksmith.utils.tools import parse_tool_from_args
 from blacksmith.utils.registry import registry
 
 
 def tool(name, description, params):
-    # Parse the tool name from sysargs
-    tool_name = parse_tool_from_args()
-
     def decorator(func):
-        # Only run business logic for target tool functions
-        if name != tool_name:
-            return func
-
-        # Start FastAPI and Redis
-        app = FastAPI()
-
-        registry.register_tool(func=func, description=description, params=params)
-
-        # Spin up the microservice
-        @functools.wraps(func)
-        def server():
-            @app.get("/")
-            async def entry(request: Request):
-                # TODO: Wrap this in a try/except block
-                body = await request.json()
-                return func(**body)
-
-            return threading.Thread(target=run_server, kwargs={"app": app})
-
-        server().start()
-        return server
+        registry.register_tool(name=name, func=func, description=description, params=params)
 
     return decorator
 
@@ -49,7 +20,3 @@ def use_tool(tool_name: str, args: dict):
 
 def get_tools():
     return registry.get_tools()
-
-
-def run_server(app):
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
--- blacksmith/utils/registry.py
+++ blacksmith/utils/registry.py
@@ -1,36 +1,27 @@
-import redis
 import json
 import requests
-from blacksmith.config.constants import REGISTRY_CONTAINER_NAME
 from blacksmith.utils.tools import tool_to_json_func
 
 
 class ToolRegistry:
     def __init__(self) -> None:
-        self.r = redis.Redis(host=REGISTRY_CONTAINER_NAME, port=6379)
+        self.tools = {}
 
-    def __del__(self) -> None:
-        self.r.close()
-
-    def register_tool(self, func, description, params):
+    def register_tool(self, name, func, description, params):
         try:
             tool_json_str = tool_to_json_func(
                 description=description, func=func, params_desc=params
             )
-            self.r.rpush("tools", tool_json_str)
+            self.tools[name] = tool_json_str
             print(f"Successfully registered {description}")
         except Exception as e:
             print(f"Error: {e}")
 
     def get_tools(self):
-        tools = [json.loads(tool.decode()) for tool in self.r.lrange("tools", 0, -1)]
-        return tools
+        return self.tools
 
     def use_tool(self, tool_name, args):
-        url = f"http://{tool_name}:5000"
-        res = requests.get(url=url, json=args)
-        data = res.json()
-        return data
+        pass
 
 
 registry = ToolRegistry()
